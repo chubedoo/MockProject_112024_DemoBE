92377613cb73e47616ff9f7b0d50a8f286905c83
khanh huyen <ngohuyen.280703@gmail.com>
2024-11-08 14:25:47 +0700

    init-base-code

---
+++ src/main/java/com/example/mockproject_112024_democrud_be/Mockproject112024DemocrudBeApplication.java
@@ -0,0 +1,13 @@
+package com.example.mockproject_112024_democrud_be;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Mockproject112024DemocrudBeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Mockproject112024DemocrudBeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/mockproject_112024_democrud_be/Mockproject112024DemocrudBeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.mockproject_112024_democrud_be;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Mockproject112024DemocrudBeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
